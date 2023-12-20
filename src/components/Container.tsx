dc3926f409ed718c3726aaf38f926b3a8e146c6e
Jhonatan Lemes <jjjhonline@gmail.com>
2023-12-20 11:59:44 +0000

    first commit

---
+++ src/components/Container.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+interface Props{
+    children: ReactNode
+}
+
+export default function Container({children}: Props){
+    return(
+        <div className="flex items-center justify-between w-full max-w-[1246px] px-[15px] mx-auto">{children}</div>
+    )
+}
